c5a0c3fcff3bfbdb8894a7885f0d4ebd47a78444
Colin Marshall <colin.marshall94@gmail.com>
2022-11-21 22:32:37 -0800

    pseudocode

---
+++ Sample_README.md
@@ -0,0 +1,24 @@
+# TITLE
+
+## Table of Contents
+
+- [Description](#Description)
+- [Usage](#Usage)
+- [License](#license)
+- [Badges](#badges)
+
+## Description
+
+lorem ipsum
+
+## Usage
+
+how do?
+
+## License
+
+import license obj
+
+## Badges
+
+include some badges
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // TODO: Include packages needed for this application
+const { notStrictEqual, notEqual } = require('assert');
 const fs = require('fs')
 const inq = require('inquirer')
 const utils = require("./utils/generateMarkdown")
 
-console.log(utils)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your project name?', 'Describe your project:', 'Installation Instructions:','Usage Information:','Contribution Guidelines:','Test Instructions:'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+
+// pseudo and notes 
+// const template = `# TITLE\n\n## Table of Contents\n\n- [Description](#Description)\n- [Usage](#Usage)\n- [License](#license)\n- [Badges](#badges)\n\n## Description\n\nlorem ipsum\n\n## Usage\n\nhow do?\n\n## License\n\nimport license obj\n\n## Badges\n\ninclude some badges`;
+
+//     fs.writeFile("Sample_README.md", template, (err) =>
+//       err ? console.error(err) : console.log("Your README is ready!")
+//     );
